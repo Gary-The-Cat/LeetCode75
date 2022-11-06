dd9c8a8bc04ca7638ce2a1b819d6a4cf38b47262
GaryTheCat <lukeberry9919@gmail.com>
2022-11-06 14:47:59 +1030

    Unoptimized 125 - Valid Palindrome

---
+++ Blind75/CSharpTestRunner/String/ValidPalindrome.cs
@@ -0,0 +1,16 @@
+﻿using _125_ValidPalindrome;
+
+namespace TestRunner.String;
+
+public class ValidPalindrome
+{
+    public class SumOfTwoIntegers
+    {
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        [InlineData(" ", true)]
+        public void Run(string potentialPalindrome, bool isPalindrome) =>
+            PalindromeValidator.IsPalindrome(potentialPalindrome).Should().Be(isPalindrome);
+    }
+}
---
+++ Blind75/_125_ValidPalindrome/PalindromeValidator.cs
@@ -0,0 +1,25 @@
+﻿namespace _125_ValidPalindrome;
+
+public class PalindromeValidator
+{
+    public static bool IsPalindrome(string s)
+    {
+        var reducedString = s.ToLower().ToCharArray().Where(c => Char.IsLetterOrDigit(c)).ToArray();
+
+        var firstIndex = 0;
+        var lastIndex = reducedString.Length - 1;
+
+        while (lastIndex > firstIndex)
+        {
+            if (reducedString[firstIndex] != reducedString[lastIndex])
+            {
+                return false;
+            }
+
+            firstIndex++;
+            lastIndex--;
+        }
+
+        return true;
+    }
+}
